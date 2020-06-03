dd3c11d99e2825a1b51004bd81ffb23274e58d8e
Ikraam <ikraamghoor@gmail.com>
2020-06-03 14:53:51 +0200

    created validation to ensure that duplicate freindships are not created

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,16 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+
+  validate :users_friends
+
+  private
+
+  def users_friends
+    if User.where(user_id: friend_id, friend_id: user_id).or(User.where(user_id: user_id, friend_id: friend_id)).exists?
+      errors.add(:user_id, 'Already friends!')
+    end
+  end
+
 end
